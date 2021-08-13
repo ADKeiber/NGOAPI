7b9d22f3e7ce0da9c4ec24065c663f99f457a05e
Austin Keiber <adkeiber@gmail.com>
2021-08-13 00:07:46 -0400

    project creation

---
+++ src/main/java/com/example/NGOAPI/NgoapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.NGOAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NGOAPI/NgoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NGOAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
