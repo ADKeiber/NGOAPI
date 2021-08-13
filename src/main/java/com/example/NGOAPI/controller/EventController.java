9e811160984fa0e05547d81db675e9a51caeb774
Austin Keiber <adkeiber@gmail.com>
2021-08-13 07:01:52 -0400

    changing pom

---
+++ src/main/java/com/example/NGOAPI/controller/EventController.java
@@ -0,0 +1,13 @@
+package com.example.NGOAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
